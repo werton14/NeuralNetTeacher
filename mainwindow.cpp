d4647854d10afa728291046bdf527fa2bb92b22c
Defaault <narutik.temik@mail.ru>
2017-09-23 16:21:57 +0300

    added initialization weight

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <time.h>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
+    setRandowValues();
     ui->setupUi(this);
+}
+
+void MainWindow::setRandowValues()
+{
+        srand(time(NULL));
+   for(int i = 0; i < 100; i++){
+
+       int random = rand() % 100;
+       double randomValues = random / 100.0;
+       qDebug()<<randomValues;
+   }
+
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
